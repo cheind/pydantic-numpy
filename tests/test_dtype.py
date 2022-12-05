ec5883c5852510e61915072853b673bc9c9eaada
Antoine DECHAUME <>
2022-12-05 11:26:29 +0100

    Mark xfails for types not available on windows

--- tests/test_dtype.py
+++ tests/test_dtype.py
@@ -1,8 +1,22 @@
+import sys
+
 import numpy as np
 import pytest
 from pydantic import BaseModel
 
 import pydantic_numpy.dtype as pnd
+
+try:
+    np_float128 = np.float128
+except AttributeError:
+    # Not available on windows.
+    np_float128 = None
+
+try:
+    np_complex256 = np.complex256
+except AttributeError:
+    # Not available on windows.
+    np_complex256 = None
 
 
 @pytest.mark.parametrize("data", (1, 1.0))
@@ -11,7 +25,12 @@ import pydantic_numpy.dtype as pnd
             (pnd.float16, np.float16),
             (pnd.float32, np.float32),
             (pnd.float64, np.float64),
-            (pnd.float128, np.float128),
+            pytest.param(
+                pnd.float128, np_float128,
+                marks=pytest.mark.skipif(
+                    sys.platform == "win32", reason="dtype is not available on windows"
+                )
+            ),
             (pnd.int8, np.int8),
             (pnd.int16, np.int16),
             (pnd.int32, np.int32),
@@ -34,7 +53,12 @@ def test_float32(data, pnp_dtype, np_dtype):
     "pnp_dtype,np_dtype", (
             (pnd.complex64, np.complex64),
             (pnd.complex128, np.complex128),
-            (pnd.complex256, np.complex256),
+            pytest.param(
+                pnd.complex256, np_complex256,
+                marks=pytest.mark.skipif(
+                    sys.platform == "win32", reason="dtype is not available on windows"
+                )
+            ),
     )
 )
 def test_complex256(data, pnp_dtype, np_dtype):
